95453de4e7edcf6ba85f89b79cf4960e08e360e3
Egor Litvinov <yegor.litvinov@yandex.ru>
2018-05-26 14:54:31 +0300

    always get 404

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/gin-contrib/sessions"
-	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-contrib/sessions/redis"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
@@ -30,7 +31,7 @@ type Todo struct {
 	Text      string    `gorm:"not null" json:"text" binding:"required"`
 	Order     uint      `gorm:"not null;unique;auto_increment" json:"order"`
 	Completed bool      `gorm:"not null" json:"completed"`
-	User      uint      `gorm:"not null" json:"user"`
+	User      uint      `gorm:"not null" json:"-"`
 	Tag       uint      `json:"tag"`
 }
 
@@ -42,8 +43,14 @@ type Tag struct {
 }
 
 func listTodos(c *gin.Context) {
+	user := getUserFromContext(c)
+	fmt.Println(user.ID)
 	var todos []Todo
-	db.Find(&todos)
+	// todos := make([]Todo, 0, 100)
+	err := db.Where(&Todo{User: user.ID}).Find(&todos).Error
+	if err != nil {
+		panic(err)
+	}
 	c.JSON(http.StatusOK, todos)
 }
 
@@ -53,22 +60,40 @@ func createTodo(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, err)
 		return
 	}
-	db.Create(&todo)
+	user := getUserFromContext(c)
+	todo.User = user.ID
+	if err := db.Create(&todo).Error; err != nil {
+		panic(err)
+	}
 	c.JSON(http.StatusCreated, todo)
 }
 
 func updateTodo(c *gin.Context) {
 	var todo Todo
-	id := c.Param("id")
-	if err := db.First(&todo, id).Error; err != nil {
-		c.JSON(http.StatusNotFound, "")
+	// get todo
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil || id < 1 {
+		c.JSON(http.StatusNotFound, gin.H{"Error": "Not found"})
 		return
 	}
+	user := getUserFromContext(c)
+	err = db.Where(&Todo{User: user.ID, ID: uint(id)}).First(&todo).Error
+	if gorm.IsRecordNotFoundError(err) {
+		c.JSON(http.StatusNotFound, gin.H{"Error": "Not found"})
+		return
+	}
+	if err != nil {
+		panic(err)
+	}
+	// validate
+	// TODO: order changes
 	if err := c.BindJSON(&todo); err != nil {
 		c.JSON(http.StatusBadRequest, err)
 		return
 	}
-	db.Save(&todo)
+	if err := db.Save(&todo).Error; err != nil {
+		panic(err)
+	}
 	c.JSON(http.StatusOK, todo)
 }
 
@@ -85,7 +110,7 @@ func login(c *gin.Context) {
 	}
 	var user User
 	if err := db.Where("email = ? and password = ?", creds.Email, creds.Password).First(&user).Error; err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"Error": "Wrong password / No such user"})
+		c.JSON(http.StatusBadRequest, gin.H{"Error": gin.H{"Tag": "Wrong password / No such user"}})
 		return
 	}
 	setSessionUser(c, user.ID)
@@ -93,13 +118,6 @@ func login(c *gin.Context) {
 }
 
 func logout(c *gin.Context) {
-	value, exist := c.Get("user")
-	if !exist {
-		panic(errors.New("session user not exists"))
-	}
-	if _, ok := value.(User); !ok {
-		panic(errors.New("user in the session is not really user"))
-	}
 	deleteSessionUser(c)
 	c.JSON(http.StatusOK, gin.H{"Info": "You are successfully logged out"})
 }
@@ -118,32 +136,35 @@ func signup(c *gin.Context) {
 	user.Email = creds.Email
 	user.Password = creds.Password
 	if err := db.Create(&user).Error; err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"Error": "User already exists"})
+		c.JSON(http.StatusBadRequest, gin.H{"Error": gin.H{"Tag": "User already exists"}})
 		return
 	}
 	setSessionUser(c, user.ID)
 	c.JSON(http.StatusCreated, gin.H{"Info": "The user successfully created"})
 }
 
-func getSessionUser(c *gin.Context) *User {
+func getSessionUser(c *gin.Context) (User, error) {
+	var user User
 	session := sessions.Default(c)
-	userID := session.Get("userID")
-	if userID == nil {
-		return nil
+	userID, ok := session.Get("userID").(uint)
+	if !ok {
+		return user, errors.New("Session is empty")
 	}
-	user := new(User)
-	if err := db.First(user, userID).Error; err != nil {
+	if err := db.First(&user, userID).Error; err != nil {
 		if gorm.IsRecordNotFoundError(err) {
-			return nil
+			return user, errors.New("User not found")
 		}
 		panic(err)
 	}
-	return user
+	return user, nil
 }
 
 func setSessionUser(c *gin.Context, userID uint) {
 	session := sessions.Default(c)
 	session.Set("userID", userID)
+	if err := session.Save(); err != nil {
+		panic(err)
+	}
 }
 
 func deleteSessionUser(c *gin.Context) {
@@ -151,10 +172,22 @@ func deleteSessionUser(c *gin.Context) {
 	session.Delete("userID")
 }
 
+func getUserFromContext(c *gin.Context) User {
+	value, exist := c.Get("user")
+	if !exist {
+		panic(errors.New("session user not exists"))
+	}
+	user, ok := value.(User)
+	if !ok {
+		panic(errors.New("user in the context is not really user"))
+	}
+	return user
+}
+
 func authRequiredMiddleware(c *gin.Context) {
-	user := getSessionUser(c)
-	if user == nil {
-		c.JSON(http.StatusUnauthorized, "")
+	user, err := getSessionUser(c)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"Error": "Unauthorized"})
 		c.Abort()
 	}
 	c.Set("user", user)
@@ -176,21 +209,20 @@ func main() {
 
 	// Session
 	var store redis.Store
-	// store, err = redis.NewStore(10, "tcp", "localhost:6379", "", []byte("LFoNBWW394@#$#d"))
-	store = cookie.NewStore([]byte("LFoNBWW394@#$#d"))
+	store, err = redis.NewStore(10, "tcp", "localhost:6379", "", []byte("LFoNBWW394@#$#d"))
 	if err != nil {
 		panic(err)
 	}
+	store.Options(sessions.Options{MaxAge: 60 * 60 * 1})
 
 	// Routes/Middleware
 	router := gin.Default()
 	sessionMiddleware := sessions.Sessions("x-session", store)
-	// router.Use()
+	router.Use(sessionMiddleware)
 
 	apiGroup := router.Group("/api/v1/")
 	todoGroup := apiGroup.Group("/todos/", authRequiredMiddleware)
 	authGroup := apiGroup.Group("/auth/")
-	authGroup.Use(sessionMiddleware)
 
 	authGroup.POST("/login/", login)
 	authGroup.POST("/signup/", signup)
